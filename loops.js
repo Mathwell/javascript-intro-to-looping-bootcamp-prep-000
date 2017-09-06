2293c2934ed83f5cd0534cc77beb692d4bcb3bbb
Olena Ageyeva <eageeva@hotmail.com>
2017-09-06 03:09:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,7 +19,7 @@ function whileLoop(n){
 function doWhileLoop(array){
   do{
     array.shift()
-  }while((maybeTrue()) and (array.length>0))
+  }while(maybeTrue() and array.length>0)
   return(array)
 }
 
